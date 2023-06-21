d64600618c3f2dcbfbd31f899cc86f4cddcca0fe
Cedrick Takacs <cedtakacs@outlook.com>
2023-06-21 13:44:30 -0400

    Price CAD

--- script.js
+++ script.js
@@ -27,28 +27,63 @@ function searchCard() {
     });
   }
   
+  function convertUSDtoCAD(usdPrice) {
+    // Replace 'YOUR_APP_ID' with your actual app ID or use a different currency conversion service
+    const appId = '6eb31aae1bcc4226aff9be25733ec63f';
+    const apiUrl = `https://openexchangerates.org/api/latest.json?app_id=${appId}`;
+  
+    return fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        if (data.rates && data.rates.CAD) {
+          const cadRate = data.rates.CAD;
+          const cadPrice = (usdPrice * cadRate).toFixed(2);
+          return cadPrice;
+        } else {
+          throw new Error('Conversion rate not available.');
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        return 'N/A';
+      });
+  }
+  
   function createCardDetailsElement(card) {
     const cardDetails = document.createElement('div');
     cardDetails.className = 'card-details';
   
     const variationHTML = createVariationsHTML(card);
   
-    const normalPrice = card.prices.usd || 'N/A';
-    const foilPrice = card.prices.usd_foil || 'N/A';
+    const normalPriceUSD = card.prices.usd || 'N/A';
+    const foilPriceUSD = card.prices.usd_foil || 'N/A';
   
-    cardDetails.innerHTML = `
-      <h2>${card.name}</h2>
-      <img src="${card.image_uris.normal}" alt="${card.name}">
-      <p><strong>Type:</strong> ${card.type_line}</p>
-      <p><strong>Rarity:</strong> ${card.rarity}</p>
-      <p><strong>Set:</strong> ${card.set_name}</p>
-      <p><strong>Artist:</strong> ${card.artist}</p>
-      <p><strong>Price:</strong> $${normalPrice} / $${foilPrice} (Foil)</p>
-      ${variationHTML}
-    `;
+    // Convert USD prices to CAD
+    convertUSDtoCAD(normalPriceUSD)
+      .then(cadPrice => {
+        const normalPriceCAD = cadPrice !== 'N/A' ? `$${cadPrice} CAD` : 'N/A';
+  
+        convertUSDtoCAD(foilPriceUSD)
+          .then(cadFoilPrice => {
+            const foilPriceCAD = cadFoilPrice !== 'N/A' ? `$${cadFoilPrice} CAD` : 'N/A';
+  
+            cardDetails.innerHTML = `
+              <h2>${card.name}</h2>
+              <img src="${card.image_uris.normal}" alt="${card.name}">
+              <p><strong>Type:</strong> ${card.type_line}</p>
+              <p><strong>Rarity:</strong> ${card.rarity}</p>
+              <p><strong>Set:</strong> ${card.set_name}</p>
+              <p><strong>Artist:</strong> ${card.artist}</p>
+              <p><strong>Price:</strong> $${normalPriceUSD} USD / ${normalPriceCAD}</p>
+              <p><strong>Price Foil:</strong> ${foilPriceUSD} USD / ${foilPriceCAD})</p>
+              ${variationHTML}
+            `;
+          });
+      });
   
     return cardDetails;
   }
+  
   
   
   function createVariationsHTML(card) {
